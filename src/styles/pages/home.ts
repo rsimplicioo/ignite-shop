6229147094183aae500346a0063c39e28f14530c
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-07-13 11:19:59 -0300

    feat: configurando getStaticProps na pagina parametrizada de produtos

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,18 +1,18 @@
 import { styled } from "..";
 
+import Link from "next/link";
+
 export const HomeContainer = styled("main", {
   display: "flex",
-  //   gap: "3rem",
   width: "100%",
   maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
   marginLeft: "auto",
   minHeight: 656,
 });
 
-export const Product = styled("a", {
-  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%);",
+export const Product = styled(Link, {
+  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  //   padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
   overflow: "hidden",
@@ -34,23 +34,24 @@ export const Product = styled("a", {
 
     borderRadius: 6,
 
-    transform: "translateY(110%)",
-    opacity: 0,
-    transition: "all 0.2s ease-in-out",
-
     display: "flex",
     alignItems: "center",
     justifyContent: "space-between",
 
     backgroundColor: "rgba(0, 0, 0, 0.6)",
 
+    transform: "translateY(110%)",
+    opacity: 0,
+    transition: "all 0.2s ease-in-out",
+
     strong: {
       fontSize: "$lg",
+      color: "$gray100",
     },
 
     span: {
-      fontSize: "$xg",
-      fontHeight: "bold",
+      fontSize: "$xl",
+      fontWeight: "bold",
       color: "$green300",
     },
   },
